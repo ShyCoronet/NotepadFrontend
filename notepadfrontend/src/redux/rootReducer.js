7abed28e62be362fa2e861f2b038c9c4fd989c4d
Maksim Chegulov <mr.chegulov@gmail.com>
2020-09-25 13:47:45 +0300

    Added userReducer and appReducer

--- notepadfrontend/src/redux/rootReducer.js
+++ notepadfrontend/src/redux/rootReducer.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import { notesReducer } from "./noteReducer";
+import { userReducer } from "./userReducer";
+import {appReducer} from "./appReducer";
 
 export const rootReducer = combineReducers({
-    notes: notesReducer
+    notes: notesReducer,
+    user: userReducer,
+    app: appReducer
 })
