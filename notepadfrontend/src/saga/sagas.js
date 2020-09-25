64d2cd2d1e14e322991840aaeda4ff64fa89c4ae
Maksim Chegulov <mr.chegulov@gmail.com>
2020-09-25 13:45:05 +0300

    Added loader and getUser generator

--- notepadfrontend/src/saga/sagas.js
+++ notepadfrontend/src/saga/sagas.js
@@ -1,20 +1,27 @@
 import { takeEvery, call, put, debounce } from 'redux-saga/effects'
-import { ADD_NOTES, REQUEST_NOTES, ADD_NOTE, REQUEST_NOTE, DELETE_NOTE, REQUEST_FOR_DELETE_NOTE, UPDATE_NOTE, REQUEST_FOR_UPDATE_NOTE } from '../redux/types'
-import { requestNotes } from '../services/api/NotesApi/requestNotes'
-import { requestNote } from '../services/api/NotesApi/requestNote'
-import { requestForDeleteNote } from '../services/api/NotesApi/requestForDeleteNote'
-import { requestForUpdateNote } from '../services/api/NotesApi/requestForUpdateNote'
+import { ADD_NOTES, REQUEST_NOTES, ADD_NOTE, REQUEST_NOTE, 
+DELETE_NOTE, REQUEST_FOR_DELETE_NOTE, UPDATE_NOTE, REQUEST_FOR_UPDATE_NOTE, 
+ADD_USER, REQUEST_USER} from '../redux/types'
+import { requestNotes } from '../api/NotesApi/requestNotes'
+import { requestNote } from '../api/NotesApi/requestNote'
+import { requestForDeleteNote } from '../api/NotesApi/requestForDeleteNote'
+import { requestForUpdateNote } from '../api/NotesApi/requestForUpdateNote'
+import { requestUser } from '../api/UserApi/requestUser'
+import {hideLoader, showLoader} from "../redux/actions";
 
 export function* sagaWatcher() {
    yield takeEvery(REQUEST_NOTES, getNotes)
    yield takeEvery(REQUEST_NOTE, getNote)
    yield takeEvery(REQUEST_FOR_DELETE_NOTE, deleteNote)
    yield debounce(500, REQUEST_FOR_UPDATE_NOTE, updateNote)
+   yield takeEvery(REQUEST_USER, getUser)
 }
 
 function* getNotes() {
+    yield put(showLoader())
     const payload = yield call(requestNotes) 
     yield put({type : ADD_NOTES, payload })
+    yield put(hideLoader())
 }
 
 function* getNote() {
@@ -30,4 +37,14 @@ function* deleteNote(action) {
 function* updateNote(action) {
     const payload = yield call(requestForUpdateNote, action.updatedNote)
     yield put({type: UPDATE_NOTE, payload})
+}
+
+function* getUser() {
+    try {
+        const payload = yield call(requestUser)
+        yield put({type: ADD_USER, payload})
+    }
+    catch(e) {
+        console.log(e)
+    }
 }
