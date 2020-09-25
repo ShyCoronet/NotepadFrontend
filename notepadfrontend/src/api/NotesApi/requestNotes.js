6e6640bc77d2b748a0d9ebc103f6071c3a9294c8
Maksim Chegulov <mr.chegulov@gmail.com>
2020-09-25 13:48:32 +0300

    Added function to query user notes

---
+++ notepadfrontend/src/api/NotesApi/requestNotes.js
@@ -0,0 +1,10 @@
+import { fetchWithAuth } from '../../Authentication'
+
+export async function requestNotes() {
+    const response = await fetchWithAuth('https://localhost:44321/api/notes', 
+        {
+            method: 'GET',
+            headers: {'Content-Type' : 'application/json'}
+        })
+    return await response.json()
+}
