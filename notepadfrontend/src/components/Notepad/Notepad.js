b39107e3c3b26f1999e61b0b01dbb9cdce2de3e4
Maksim Chegulov <mr.chegulov@gmail.com>
2020-09-25 13:38:04 +0300

    Added Loader component, SortBar component, LogoutButton component sorting notes logic

--- notepadfrontend/src/components/Notepad/Notepad.js
+++ notepadfrontend/src/components/Notepad/Notepad.js
@@ -2,40 +2,64 @@ import React, { useEffect } from 'react'
 import './style.css'
 import CreateNoteButton from '../CreateNoteButton/CreateNoteButton'
 import SearchBar from '../SearchBar/SearchBar'
-import NoteEditor from '../TextEditor/NoteEditor'
+import NoteEditor from '../NoteEditor/NoteEditor'
 import NoteList from '../NoteList/NoteList'
 import { useDispatch, useSelector } from 'react-redux'
-import { getNotes } from '../../redux/actions'
+import {getNotes, getUser} from '../../redux/actions'
+import SortBar from "../SortBar/SortBar";
+import {getComparer} from "../../utils/noteCompares";
+import Loader from "../Loader/Loader";
+import LogoutButton from "../LogoutButton/LogoutButton";
 
 
 export default function Notepad() {
-
     const dispatch = useDispatch()
+
+    const user = useSelector(state => state.user.user)
+
+    const sortType = useSelector(state => state.notes.noteSortPredicate)
+    const searchValue = useSelector(state => state.notes.searchBarValue)
+    const noteComparer = getComparer(sortType)
+
+    const loading = useSelector(state => state.app.loading)
+
     const notes = useSelector(state => state.notes.notes)
+    const filteredAndSortedNotes = searchValue !== null ? notes.filter(note =>
+            note.name.toLowerCase().includes(searchValue)).sort(noteComparer)
+        : notes.sort(noteComparer)
+
     const activeNoteId = useSelector(state => state.notes.activeNoteId)
     const activeNote = notes.find(note => note.noteId === activeNoteId)
 
 
+
     useEffect(() => {
+        dispatch(getUser())
         dispatch(getNotes())
     }, [])
-    
+
     return(
        <div className='notepad'>
            <div className='header'>
                <h1 className='title'>Notepad</h1>
+               <div className='right-menu-container'>
+                   <label className='user-login'>{user.login}</label>
+                   <LogoutButton/>
+               </div>
             </div>
             <div className='main'>
                 <div className='side-bar'>
                     <CreateNoteButton/>
                     <SearchBar/>
-                    <hr className="separator"></hr>
-                    <NoteList notes={notes}/>
+                    <SortBar/>
+                    <hr className="separator"/>
+                    {loading ? <Loader/>
+                    :<NoteList notes={filteredAndSortedNotes}/>}
                 </div>
             {activeNote !== undefined
             ? <NoteEditor key={activeNote.noteId} 
             activeNote={activeNote}/> 
-            : <div></div>}
+            : <div/>}
         </div>
        </div>
     )
