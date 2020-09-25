16c525b2f58d40251f445d62e291401c6363bd7d
Maksim Chegulov <mr.chegulov@gmail.com>
2020-09-25 13:21:40 +0300

    Added actions: getUser, deleteUser, setSortType, setSearchValue, showLoader, hideLoader

--- notepadfrontend/src/redux/actions.js
+++ notepadfrontend/src/redux/actions.js
@@ -1,4 +1,12 @@
-import { SET_ACTIVE_NOTE, REQUEST_FOR_DELETE_NOTE, REQUEST_NOTES, REQUEST_NOTE, REQUEST_FOR_UPDATE_NOTE } from './types'
+import {
+    SET_ACTIVE_NOTE,
+    REQUEST_FOR_DELETE_NOTE,
+    REQUEST_NOTES,
+    REQUEST_NOTE,
+    REQUEST_FOR_UPDATE_NOTE,
+    REQUEST_USER,
+    SET_SORT_TYPE, SET_SEARCH_VALUE, SHOW_LOADER, HIDE_LOADER, DELETE_USER
+} from './types'
 
 export function getNotes() {
     return {
@@ -27,5 +35,41 @@ export function setActiveNote(id) {
 export function updateNote(updatedNote) {
     return {
         type: REQUEST_FOR_UPDATE_NOTE, updatedNote
+    }
+}
+
+export function getUser() {
+    return {
+        type: REQUEST_USER
+    }
+}
+
+export function deleteUser() {
+    return {
+        type: DELETE_USER
+    }
+}
+
+export function setSortType(sortType) {
+    return {
+        type: SET_SORT_TYPE, payload: sortType
+    }
+}
+
+export function setSearchValue(searchValue) {
+    return {
+        type: SET_SEARCH_VALUE, payload: searchValue
+    }
+}
+
+export function showLoader() {
+    return {
+        type: SHOW_LOADER
+    }
+}
+
+export function hideLoader() {
+    return {
+        type: HIDE_LOADER
     }
 }
